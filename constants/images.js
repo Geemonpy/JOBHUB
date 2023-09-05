26c11a6551957294a69b3a5130ad97d4fd0e1309
Geemon P Y <geemonpy@Geemons-MacBook-Air.local>
2023-09-05 22:44:06 +0530

    Initial commit

---
+++ constants/images.js
@@ -0,0 +1,7 @@
+import profile from "../assets/images/gee.jpeg";
+import logo from "../assets/images/gee.jpeg";
+
+export default {
+  profile,
+  logo,
+};
---
+++ hook/useFetch.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const apiKey = "cde18d05e9msh8b35a5e188aa960p161797jsn998786d84d75"; // Replace with your actual API key
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": apiKey,
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
